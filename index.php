1ddec09b7db95ec0a7de4007ebce91462018043b
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2018-10-09 16:03:51 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+$method = $_SERVER['REQUEST_METHOD'];
+//process only when method id post
+if($method == 'POST')
+{
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+	
+	
+
+			$opts = array();
+			$opts['http'] = array();
+			$opts['http']['method']="GET";
+			$opts['http']['header']="Accept-language: en\r\n"."Cookie: foo=bar\r\n";
+
+			$t1=stream_context_create($opts);
+		
+			// Open the file using the HTTP headers set above
+			$test_file=file_get_contents("https://efashion-r.herokuapp.com/?appid=2d1e87e7-501b-410a-b365-98356344bd82", false, $t1);
+			
+			$file = json_decode($test_file);
+			$speech_data = $file->results->result->fulfillment->speech;
+			
+			
+			
+				
+			
+		
+	
+	
+		
+			
+	
+	$response = new \stdClass();
+    	$response->fulfillmentText = speech_data;
+    	$response->source = "webhook";
+	echo json_encode($response);
+}
+else
+{
+	echo "Method not allowed";
+}
+?>
