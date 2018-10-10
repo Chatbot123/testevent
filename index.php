d9dc9bc3bcbb2d9a5e959f40ab3287bceb994351
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2018-10-10 10:22:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ if($method == 'POST')
 	$json = json_decode($requestBody);
 	
 	$sec = $json->queryResult->parameters->sec;
-	echo time();
-	echo "\r\n";
+	
+	
 	$nexttick=time()+$sec;
-	echo $nexttick;
+	//echo $nexttick;
 	$active=true;
 
 	while ($active)
@@ -33,9 +33,9 @@ if($method == 'POST')
 		 $active=false;	
     	}
 
-  	
+	}
   
-}
+
 	 
 	 
 			
